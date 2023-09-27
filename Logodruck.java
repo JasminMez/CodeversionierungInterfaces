f5c86cd6a97d24958a3c5977026a4364d4fe758e
@Kathleiya <kaschimpfoessl@tsn.at>
2023-09-27 10:38:12 +0200

    neue Änderung

--- Logodruck.java
+++ Logodruck.java
@@ -8,12 +8,13 @@ public class Logodruck implements IDruckbar
     @Override
     public void drucken(Person person)
     {
+        System.out.println("\n" + "Ihr Berater für Druck aller Art: " + person.getLastName());
         System.out.println(" ____              _              _ _            _____     _\n" +
                 "|    \\ ___ _ _ ___| |_ ___     __| | |___ ___   |  _  |___| |_\n" +
                 "|  |  |  _| | |  _| '_| -_|   |. | | | -_|  _|  |     |  _|  _|\n" +
                 "|____/|_| |___|___|_|_|___|  |___|_|_|___|_|    |__|__|_| | |\n" +
                 "                                                          |__|");
 
-        System.out.println("\n" + "Ihr Berater für Druck aller Art: " + person.getLastName());
+
     }
 }
